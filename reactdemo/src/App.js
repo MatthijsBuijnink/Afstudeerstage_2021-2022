b8a159de2a6246bea38047d6144088714dfabbb1
mathijs <mwo.buijnink@student.han.nl>
2022-02-02 10:08:44 +0100

    React demo

---
+++ reactdemo/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import {Sidebar} from "./components/Sidebar";
+import {Step} from "./components/Step";
+
+function App() {
+  return (
+    <div className="App">
+        <Step />
+        <Sidebar />
+    </div>
+  );
+}
+
+export default App;
